bb458a6f45dd3b5238b7843cb4e8d44c43907545
YuriMont <yuri.mont1043@gmail.com>
2024-04-08 20:41:36 -0300

    auteracoes nos objetos e em css

--- src/data/PartnersData.js
+++ src/data/PartnersData.js
@@ -1,35 +1,10 @@
-import uepb from '../assets/images/parceiros/u__1_-removebg-preview.png';
-import eletrolaser from '../assets/images/parceiros/eletrolaser.png';
-import af from '../assets/images/parceiros/af.png'
-import alain from '../assets/images/parceiros/alain.png'
-import artway from '../assets/images/parceiros/artway.png'
-import casacactus from '../assets/images/parceiros/casacactus.png'
-import determinacao from '../assets/images/parceiros/determinacao.png'
-import esus from '../assets/images/parceiros/esus.png'
-import fera from '../assets/images/parceiros/fera.png'
-import flocao from '../assets/images/parceiros/flocao.png'
-import graficaandyara from '../assets/images/parceiros/graficaandyara.png'
-import guedes from '../assets/images/parceiros/guedes.png'
-import mio from '../assets/images/parceiros/mio.png'
-import moinhhopatoense from '../assets/images/parceiros/moinhhopatoense.png'
-import papelariaandyara from '../assets/images/parceiros/papelariaandyara.png'
-import papelariapatoense from '../assets/images/parceiros/papelariapatoense.png'
-import pda from '../assets/images/parceiros/pda.png'
-import prestcontas from '../assets/images/parceiros/prestcontas.png'
-import queiroz from '../assets/images/parceiros/queiroz.png'
-import rainha from '../assets/images/parceiros/rainha.png'
-import sertaodesenvolve from '../assets/images/parceiros/sertaodesenvolve.png'
-import servtec from '../assets/images/parceiros/servtec.png'
-import tocaraposa from '../assets/images/parceiros/tocaraposa.png'
-import urbano from '../assets/images/parceiros/urbano.png'
-import vanessa from '../assets/images/parceiros/vanessa.png'
-
 const partnersData = [
-    {
-        href: "https://www.instagram.com/uepboficial/",
-        image: uepb,
-        alt: "UEPB"
-    },
+    // {
+    //     // href: "",
+    //     // image: ,
+    //     // alt: ""
+    // },
+
 ];
 
 export default partnersData;
