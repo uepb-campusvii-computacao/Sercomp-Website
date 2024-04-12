b4e09d17fef95c64b330b35d898f3da6e0d18dec
YuriMont <yuri.mont1043@gmail.com>
2024-04-12 18:45:23 -0300

    responsividade na pagina edicoes anteriores

--- src/pages/PreviousEditions/2023/schedule.jsx
+++ src/pages/PreviousEditions/2023/schedule.jsx
@@ -1,105 +1,46 @@
+import rosangela from "../../../assets/images/professores/rosangela.png";
+import janderson from "../../../assets/images/professores/janderson.png";
+import jannayna from "../../../assets/images/professores/jannayna.png";
+import filipe from "../../../assets/images/participacoes/filipe.png";
+import luanderson from "../../../assets/images/participacoes/luanderson.png";
+import karliane from "../../../assets/images/participacoes/karliane.png";
+import cleisson from "../../../assets/images/participacoes/cleisson.png";
+import not_found from "../../../assets/images/image-not-found.png";
+
 const programacao = [
-  {
-    date: (
-      <>
-        21/<small>MAIO</small>
-      </>
-    ),
-    day: "1º Dia",
-    events: [
-      {
-        title: "CREDENCIAMENTO",
-        time: "07:00 - 08:00",
-        timeStr: "MANHÃ",
-        location: "Hall de Entrada da UEPB",
-      },
-      {
-        title: "CERIMONIAL DE ABERTURA",
-        time: "08:00 - 08:30",
-        timeStr: "MANHÃ",
-        location: "Auditório Celso Furtado",
-      },
-      {
-        title: "APRESENTAÇÃO CULTURAL I",
-        time: "08:30 - 09:00",
-        timeStr: "MANHÃ",
-        location: "Auditório Celso Furtado",
-      },
-      {
-        title:
-          "PALESTRA I - Trilhando o Caminho da Web 3.0: Conectando Culturas e Transformando Vidas",
-        time: "09:00 - 10:00",
-        timeStr: "MANHÃ",
-        location: "Auditório Celso Furtado",
-      },
-      { title: "MINICURSOS", time: "10:00 - 11:30", timeStr: "MANHÃ" },
-      { title: "Intervalo Almoço", time: "11:30 - 13:00", timeStr: "TARDE" },
-      {
-        title: "Maratona de Programação",
-        time: "13:00 - 18:00",
-        timeStr: "TARDE",
-      },
-      {
-        title: "CREDENCIAMENTO",
-        time: "18:00 - 19:00",
-        timeStr: "TARDE",
-        location: "Hall de Entrada da UEPB",
-      },
-      {
-        title:
-          "Painel I - Desafios e Oportunidades da Ciência de Dados: Navegando pelas Fronteiras da Inovação e Aplicações Práticas",
-        time: "19:00 - 21:00",
-        timeStr: "NOITE",
-        location: "Auditório Celso Furtado",
-      },
-      {
-        title: "Apresentação Cultural II",
-        time: "21:00 - 21:30",
-        timeStr: "NOITE",
-        location: "Auditório Celso Furtado",
-      },
-    ],
-  },
   {
     date: (
       <>
         22/<small>MAIO</small>
       </>
     ),
-    day: "2º Dia",
+    day: "1º Dia",
     events: [
       {
-        title:
-          "CREDENCIAMENTO (Participantes da Feira Tecnológica e do Hackathon)",
-        time: "07:00 - 08:00",
-        timeStr: "MANHÃ",
+        title: "CREDENCIAMENTO",
+        time: "18:00 - 19:00",
+        timeStr: "NOITE",
         location: "Hall de Entrada da UEPB",
       },
       {
-        title:
-          "Exposição Tecnológica e Demonstrações de Soluções / Feira Tecnológica / II Mostra de Estágio em Computação / Hackathon (Abertura/Brainstorming)",
-        time: "08:00 - 11:30",
-        timeStr: "MANHÃ",
-      },
-      { title: "Intervalo", time: "11:30 - 14:00", timeStr: "TARDE" },
-      {
-        title:
-          "Apresentação dos Artigos Científicos / Hackathon (Desenvolvimento)",
-        time: "14:00 - 17:00",
-        timeStr: "TARDE",
-      },
-      { title: "Intervalo", time: "17:00 - 18:30", timeStr: "TARDE" },
-      {
-        title:
-          "Painel III - Negócios em Rede: Estratégias de Inovação no Cenário de Transformação Digital",
-        time: "18:30 - 20:00",
+        title: "CERIMONIAL",
+        time: "19:00 - 19:15",
         timeStr: "NOITE",
         location: "Auditório Celso Furtado",
       },
       {
-        title: "PitchDay (Apresentação)",
-        time: "20:00 - 21:30",
+        title: "APRESENTAÇÃO CULTURAL I",
+        time: "19:15 - 19:30",
         timeStr: "NOITE",
+        location: "Auditório Celso Furtado",
+        participants: [{ name: "Lilyane Dias", photo: not_found }],
+      },
+      {
+        title: "PALESTRA I",
+        time: "19:30 - 21:00",
+        timeStr: "NOITE",
+        location: "Auditório Celso Furtado",
+        participants: [{ name: "Cleisson Ramos", photo: cleisson }],
       },
     ],
   },
@@ -109,24 +50,76 @@ const programacao = [
         23/<small>MAIO</small>
       </>
     ),
-    day: "3º Dia",
+    day: "2º Dia",
     events: [
       {
-        title:
-          "Painel II - Deserto Digital: Estratégias das Universidades para um Mundo mais Conectado",
+        title: "CREDENCIAMENTO",
+        time: "07:00 - 08:00",
+        timeStr: "MANHÃ",
+        location: "Hall de Entrada da UEPB",
+      },
+      {
+        title: "PALESTRA II",
         time: "08:00 - 09:30",
         timeStr: "MANHÃ",
         location: "Auditório Celso Furtado",
+        participants: [{ name: "Karliane Vale", photo: karliane }],
       },
-      { title: "Oficinas", time: "09:30 - 11:30", timeStr: "MANHÃ" },
-      { title: "Intervalo", time: "11:30 - 14:00", timeStr: "TARDE" },
       {
-        title: "Hackathon (Apresentação)",
-        time: "14:00 - 17:00",
-        timeStr: "TARDE",
+        title: "MINICURSOS",
+        time: "09:30 - 11:30",
+        timeStr: "MANHÃ",
+        location: "Salas Alocadas",
       },
-      { title: "Intervalo", time: "17:00 - 18:30", timeStr: "TARDE" },
-      { title: "Workshops Técnicos", time: "18:30 - 21:30", timeStr: "NOITE" },
+      {
+        title: "OFICINAS",
+        time: "14:00 - 16:00",
+        timeStr: "TARDE",
+        location: "Salas Alocadas",
+      },
+      {
+        title: "APRESENTAÇÃO DE ARTIGOS CIENTÍFICOS",
+        time: "16:00 - 17:00",
+        timeStr: "TARDE",
+        location: "Bloco A",
+      },
+      {
+        title: "CREDENCIAMENTO",
+        time: "18:00 - 19:00",
+        timeStr: "NOITE",
+        location: "Hall de Entrada da UEPB",
+      },
+      {
+        title: "APRESENTAÇÃO CULTURAL II",
+        time: "18:30 - 19:00",
+        timeStr: "NOITE",
+        location: "Auditório Celso Furtado",
+      },
+      {
+        title: "MESA REDONDA",
+        time: "19:00 - 21:00",
+        timeStr: "NOITE",
+        location: "Auditório Celso Furtado",
+        participants: [
+          { name: "Rosângela Medeiros", photo: rosangela },
+          { name: "Janderson Aguiar", photo: janderson },
+          { name: "Jannayna Domingues", photo: jannayna },
+          { name: "Filipe Andrade", photo: filipe },
+          { name: "Luanderson Silva", photo: luanderson },
+        ],
+      },
+      {
+        title: "SORTEIO DO TEMA DO HACKDAY",
+        time: "21:00 - 21:15",
+        timeStr: "NOITE",
+        location: "Auditório Celso Furtado",
+      },
+      {
+        title: "APRESENTAÇÃO CULTURAL III",
+        time: "21:15 - 21:30",
+        timeStr: "NOITE",
+        location: "Auditório Celso Furtado",
+      },
     ],
   },
   {
@@ -135,16 +128,56 @@ const programacao = [
         24/<small>MAIO</small>
       </>
     ),
-    day: "4º Dia",
+    day: "3º Dia",
     events: [
       {
-        title:
-          "Assembleia, premiação (artigos, hackathon, pitchday) e encerramento",
-        time: "08:00 - 11:30",
+        title: "HACKDAY(ABERTURA)",
+        time: "08:00 - 08:30",
         timeStr: "MANHÃ",
+        location: "Laboratório (40)",
       },
-      { title: "Intervalo", time: "11:30 - 18:30", timeStr: "TARDE" },
-      { title: "Noite Nerd", time: "18:30 - 21:30", timeStr: "NOITE" },
+      {
+        title: "FEIRA TECNOLÓGICA",
+        time: "08:30 - 11:30",
+        timeStr: "MANHÃ",
+        location: "Quadra Poliesportiva",
+      },
+      {
+        title: "HACKDAY (DESENVOLVIMENTO)",
+        time: "08:30 - 16:00",
+        timeStr: "MANHÃ/TARDE",
+        location: "Laboratório (40)",
+      },
+      {
+        title: "HACKDAY(APRESENTAÇÃO / AVALIAÇÃO)",
+        time: "16:00 - 17:00",
+        timeStr: "TARDE",
+        location: "Auditório Celso Furtado",
+      },
+      {
+        title: "PREMIAÇÃO DOS ARTIGOS",
+        time: "18:00 - 18:10",
+        timeStr: "NOITE",
+        location: "Guedes",
+      },
+      {
+        title: "PREMIAÇÃO DO HACKDAY",
+        time: "18:10 - 18:20",
+        timeStr: "NOITE",
+        location: "Guedes",
+      },
+      {
+        title: "ENCERAMENTO DO EVENTO",
+        time: "18:30 - 18:40",
+        timeStr: "NOITE",
+        location: "Guedes",
+      },
+      {
+        title: "NOITE NERD",
+        time: "18:40 - 22:00",
+        timeStr: "NOITE",
+        location: "Guedes",
+      },
     ],
   },
 ];
