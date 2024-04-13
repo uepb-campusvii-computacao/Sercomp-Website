8243013062f577ae1378c38c7a709d3ed36c2396
Lucas Henrique <henrique.lucas521@gmail.com>
2024-04-13 16:08:37 -0300

    add new activities page, new activitie card component and install a new icons lib heroicons; missing: responsivity

--- src/pages/Activities/Activities.jsx
+++ src/pages/Activities/Activities.jsx
@@ -1,9 +1,20 @@
-const Atividades = () => {
+import ActivitieCard from "../../components/ActivitieCard/ActivitieCard";
+import { activitiesCards } from "../../data/ActivitieCardsList";
+
+import styles from './Activities.module.css'
+
+const Activities = () => {
   return (
-    <section>
+    <section className={`${styles.activitiesContainer}`}>
       <h1>Atividades</h1>
+
+      <section className={`${styles.activitiesCardsSection}`}>
+        {activitiesCards.map((activitieCard) => (
+          <ActivitieCard icon={activitieCard.icon} title={activitieCard.title} text={activitieCard.text} url={activitieCard.url} key={activitieCard.url} />
+        ))}
+      </section>
     </section>
   );
 }
 
-export default Atividades;
+export default Activities;
