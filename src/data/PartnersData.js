71d7772f289590a80c2b934e989dc26630c5456c
LucasLevingston <lucaolevingston@outlook.com>
2024-04-18 17:08:40 -0300

    Adicionado informações dos parceiros

--- src/data/PartnersData.js
+++ src/data/PartnersData.js
@@ -1,9 +1,40 @@
+import guedes from '../assets/images/parceiros/guedes.png'
+import eletrolaser from '../assets/images/parceiros/eletrolaser.png'
+import moinhopatoense from '../assets/images/parceiros/moinhopatoense.png'
+import papelariapatoense from '../assets/images/parceiros/papelariapatoense.png'
+import nutrimassa from '../assets/images/parceiros/nutrimassa.png'
+
 const partnersData = [
-    // {
-    //     // href: "",
-    //     // image: ,
-    //     // alt: ""
-    // },    
+    {
+        href: "https://www.instagram.com/eletrolaser/?igshid=NTc4MTIwNjQ2YQ%3D%3D",
+        image: eletrolaser,
+        alt: "Eletrolaser"
+    },
+    {
+        href: "https://guedesshopping.com.br/",
+        image: guedes,
+        alt: "Guedes Shopping"
+    },
+    {
+        href: "https://www.instagram.com/moinho_patoense/?igshid=NTc4MTIwNjQ2YQ%3D%3D",
+        image: moinhopatoense,
+        alt: "Moinho Patoense"
+    },
+    {
+        href: "https://www.instagram.com/patoensepapelaria/?igshid=NTc4MTIwNjQ2YQ%3D%3D",
+        image: papelariapatoense,
+        alt: "Patoense Papelaria"
+    },
+    {
+        href: "a",
+        image: nutrimassa,
+        alt: "Nutrimassa"
+    },
+    {
+        href: "",
+        // image: inovatec,
+        alt: "Inovatec"
+    },
 ];
 
 export default partnersData;
