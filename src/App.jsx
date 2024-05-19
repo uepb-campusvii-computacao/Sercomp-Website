c311ec5e187d321f77d4446b8aeae08e524815d7
TidusWR <harllemnascimento@gmail.com>
2024-05-19 18:04:22 -0300

    coloca aviso de encerramento de inscrições e loja

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import DateConditional from "./components/DateConditional/DateConditional";
 import Layout from "./components/Layout/Layout";
-import { Mensagem } from "./components/Mensagem/Mensagem";
+import PartnersComponent from "./components/Partners/Partners";
 import About from "./pages/About/About";
 import Articles from "./pages/Articles/Articles";
+import Aviso from "./pages/Aviso/Aviso";
 import BuscaInscricao from "./pages/BuscaInscricao/BuscaInscricao";
 import Chamadas from "./pages/Chamadas/Chamadas";
 import Comite from "./pages/Comite/Comite";
@@ -14,8 +14,7 @@ import Error404 from "./pages/Error404/Error404";
 import Hackthon from "./pages/Hackthon/Hackthon";
 import Home from "./pages/Home/Home";
 import Ideathon from "./pages/Ideathon/Ideathon";
-import Inscricoes from "./pages/Inscricoes/Inscricoes";
-import Market from "./pages/Market/Market";
+import AllPagamentoProduto from "./pages/Pagamento/AllPagamentosProdutos";
 import Pagamento from "./pages/Pagamento/Pagamento";
 import Partners from "./pages/Partners/Partners";
 import Edition2023 from "./pages/PreviousEditions/2023";
@@ -23,7 +22,6 @@ import Privacy from "./pages/Privacy/Privacy";
 import ProgrammingMarathon from "./pages/ProgrammingMarathon/ProgrammingMarathon";
 import TechnologyFair from "./pages/TechnologyFair/TechnologyFair";
 import Timeline from "./pages/Timeline/Timeline";
-import AllPagamentoProduto from "./pages/Pagamento/AllPagamentosProdutos";
 
 function App() {
   return (
@@ -39,10 +37,19 @@ function App() {
           <Route exact path="/timeline" element={<Timeline />} />
           <Route exact path="/privacy" element={<Privacy />} />
           <Route exact path="/inscricao" element={
-            <DateConditional
-              until="2024/05/20"
-              renderIfTrue={<Inscricoes />}
-              renderIfFalse={<Mensagem texto="Inscrições encerradas!" />}
+            <Aviso
+              title="Inscrições encerradas!"
+              body={
+                <>
+                  <p>Informamos que as inscrições online para o nosso evento estão encerradas. 
+                  No entanto, ainda é possível realizar sua inscrição presencialmente no dia do evento.</p>
+
+                  <p><strong>Atenção:</strong> Para as novas inscrições feitas no local, não será possível fornecer materiais como crachá e boton.</p>
+
+                  <p>Agradecemos pela compreensão e esperamos por você!</p>
+                </>
+              }
+              append={<PartnersComponent />}
             />
           } />
           <Route exact path="/busca/inscricao/:tipo_busca?" element={<BuscaInscricao />} />
@@ -54,7 +61,19 @@ function App() {
           <Route exact path="/confirmed-activities" element={<ConfirmedActivities />} />
           <Route exact path="/programming-marathon" element={<ProgrammingMarathon />} />
           <Route exact path="/2023" element={<Edition2023 />} />
-          <Route exact path="/market" element={<Market />} />
+          <Route exact path="/market" element={
+            <Aviso
+              title="Pedidos online encerrados!"
+              body={
+                <>
+                  <p>Informamos que os pedidos online em nossa loja estão temporariamente encerrados.</p>
+
+                  <p>Agradecemos pela compreensão e esperamos por você!</p>
+                </>
+              }
+              append={<PartnersComponent />}
+            />
+          } />
           <Route exact path="/market/user/:user_id/pagamentos" element={<AllPagamentoProduto />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
